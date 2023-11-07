29e5f611b7422a72b0700d16ca3b1a154ede05d5
Melker Gustafsson <77453395+creeperdoglas@users.noreply.github.com>
2023-11-07 09:30:54 +0100

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	int I;
+	string S;
+	double F;
+	cout << "Mata in ett heltal" << endl;
+	cin >> I;
+	cout << "Du matade in hetalet" << I << endl;
+
+	cout << "Mata in ett heltal och ett ord: " << endl;
+	cin >> I;
+	cin >> S;
+	cout << "Du matade in talet |" << I << "| och ordet |" << S << "|." << endl;
+
+	cout << "Mata in fem heltal: ";
+	cin >> I;
+	cout << "Du matade in: ";
+	cout << I;
+	cout << " ";
+	cin >> I;
+	cout << I;
+	cout << " ";
+	cin >> I;
+	cout << I;
+	cout << " ";
+	cin >> I;
+	cout << I;
+	cout << " ";
+	cin >> I;
+	cout << I;
+	cout << "." << endl;
+
+	cout << "Mata in ett flyttal: ";
+	cin >> F;
+	cout << "Du skrev in talet " << F << "." << endl;
+
+	cout << "Skriv in ett heltal och ett flyttal: ";
+	cin >> I;
+	cin >> F;
+	cout << "Du skrev in heltalet ";
+	cout << setw(9) << I << endl;
+	cout << "Du skrev in flyttalet	";
+	cout << setw(5) << F << endl;
+
+	return 0;
+}
