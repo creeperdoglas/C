0a93c8aa6f132a84cf14dfc3bcd167db03979861
creeperdoglas <borenmelkan@gmail.com>
2023-11-08 14:42:38 +0100

    gg

--- lab1.cpp
+++ lab1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <cmath>
 
 using namespace std;
 
@@ -11,39 +12,43 @@ int main()
 	double F;
 	char T;
 
-	cout << "Skriv in ett heltal: ";
-	cin >> I;
-	cout << "Du skrev in heltalet " << I << endl;
+	// cout << "Skriv in ett heltal: ";
+	// cin >> I;
+	// fflush(stdin);
+	// cout << "Du skrev in heltalet " << I << endl;
 
-	cout << "skriv in fem heltal: ";
-	cin >> I;
-	cout << "Du skrev in: ";
-	cout << I;
-	cout << " ";
-	cin >> I;
-	cout << I;
-	cout << " ";
-	cin >> I;
-	cout << I;
-	cout << " ";
-	cin >> I;
-	cout << I;
-	cout << " ";
-	cin >> I;
-	cout << I;
-	cout << "." << endl;
+	// cout << "skriv in fem heltal: ";
+	// cin >> I;
+	// cout << "Du skrev in heltalen: ";
+	// cout << I;
+	// cout << " ";
+	// cin >> I;
+	// cout << I;
+	// cout << " ";
+	// cin >> I;
+	// cout << I;
+	// cout << " ";
+	// cin >> I;
+	// cout << I;
+	// cout << " ";
+	// cin >> I;
+	// fflush(stdin);
+	// cout << I;
 
-	cout << "Skriv in ett flyttal: ";
-	cin >> F;
-	cout << "Du skrev in flyttalet " << F << endl;
+	// cout << "Skriv in ett flyttal: ";
+	// cin >> F;
+	// fflush(stdin);
+	// F = std::round(F * 1000.0) / 1000.0;
+	// cout << "Du skrev in flyttalet " << F << endl;
 
 	cout << "Skriv in ett heltal och ett flyttal: ";
 	cin >> I;
 	cin >> F;
+	fflush(stdin);
 	cout << "Du skrev in heltalet ";
-	cout << setw(9) << I << endl;
-	cout << "Du skrev in flyttalet	";
-	cout << setw(9) << F << endl;
+	cout << right << setw(6) << I << endl;
+	cout << "Du skrev in flyttalet	" << std::setprecision(4) << std::fixed;
+	cout << right << setw(6) << F << endl;
 
 	cout << "Skriv in ett flyttal och ett heltal: ";
 	cin >> F;
